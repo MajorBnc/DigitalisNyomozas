34f60e0f096eeecee8b41f57fd2ffa6ec30d7bda
Major <diak@petrik.hu>
2026-01-29 14:33:52 +0100

    Timeline adatok kesz

---
+++ Timeline.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Timeline
+	{
+		private string date;
+		private string leirasEsemeny;
+
+		public Timeline(string date, string leirasEsemeny)
+		{
+			this.date = date;
+			this.leirasEsemeny = leirasEsemeny;
+		}
+
+		public string Date { get => date; set => date = value; }
+		public string LeirasEsemeny { get => leirasEsemeny; set => leirasEsemeny = value; }
+	}
+}
