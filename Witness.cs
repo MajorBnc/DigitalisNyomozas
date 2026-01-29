09aee8633630554ba88a775a6a7f1bad09bc8ef2
Major <diak@petrik.hu>
2026-01-29 14:31:01 +0100

    witness adatok kesz

---
+++ Witness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Witness
+	{
+		private Person tanu;
+		private string vallomasSzovege;
+		private string vallomasDate;
+
+		public Witness(Person tanu, string vallomasSzovege, string vallomasDate)
+		{
+			this.tanu = tanu;
+			this.vallomasSzovege = vallomasSzovege;
+			this.vallomasDate = vallomasDate;
+		}
+
+		public string VallomasSzovege { get => vallomasSzovege; set => vallomasSzovege = value; }
+		public string VallomasDate { get => vallomasDate; set => vallomasDate = value; }
+		internal Person Tanu { get => tanu; set => tanu = value; }
+	}
+}
