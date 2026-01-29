1f9f22c6eb170bf4bf132f0b854712a1ebea102f
Major <diak@petrik.hu>
2026-01-29 14:28:45 +0100

    suspect adatok kesz

---
+++ Suspect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Suspect
+	{
+		private Person gyanusitott;
+		private string vallomas;
+		private string vallomasDate;
+
+		public Suspect(Person gyanusitott, string vallomas, string vallomasDate)
+		{
+			this.gyanusitott = gyanusitott;
+			this.vallomas = vallomas;
+			this.vallomasDate = vallomasDate;
+		}
+
+		public string Vallomas { get => vallomas; set => vallomas = value; }
+		public string VallomasDate { get => vallomasDate; set => vallomasDate = value; }
+		internal Person Gyanusitott { get => gyanusitott; set => gyanusitott = value; }
+	}
+}
