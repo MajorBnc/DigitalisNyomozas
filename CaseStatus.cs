19089bd9943cdfb5f2bb8c64b6d12c5486e10cfb
Major <diak@petrik.hu>
2026-01-29 14:25:01 +0100

    CaseStatus adatok kesz

---
+++ CaseStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class CaseStatus
+	{
+		private string actualStatusz;
+
+		public CaseStatus(string actualStatusz)
+		{
+			this.actualStatusz = actualStatusz;
+		}
+
+		public string ActualStatusz { get => actualStatusz; set => actualStatusz = value; }
+	}
+}
